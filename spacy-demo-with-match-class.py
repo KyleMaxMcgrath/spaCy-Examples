bad281002390f78a7b927b17cd3a545741eea28e
Kyle McGrath <kylemcgrat@gmail.com>
2024-01-14 14:22:16 -0500

    Update spacy-demo-with-match-class.py

--- spacy-demo-with-match-class.py
+++ spacy-demo-with-match-class.py
@@ -1,55 +1,30 @@
-from typing import NamedTuple
-
-
+from typing import NamedTuple, Any, List
 import spacy
-from spacy.pipeline import EntityRuler
 from spacy.matcher import DependencyMatcher
 from spacy.util import compile_infix_regex
 from spacy.symbols import ORTH, LEMMA, POS, TAG, DEP, IS_ALPHA
+from abc import ABC, abstractmethod
+from collections import defaultdict
 
 
-class TokenizerCustomizer:
-    def __init__(self, nlp):
-        self.nlp = nlp
-
-    def add_infix_pattern(self, pattern):
-        infixes = pattern
-        infix_regex = compile_infix_regex(infixes)
-        self.nlp.tokenizer.infix_finditer = infix_regex.finditer
-
-    def add_special_case(self, word, case):
-        self.nlp.vocab.strings.add(word)
-        self.nlp.tokenizer.add_special_case(word, case)
+class Model(ABC):
+    @abstractmethod
+    def predict(self) -> Any:
+        """
+        Abstract method to be overridden in subclasses.
+        Should return a prediction in a flexible format (list, tensor, etc.).
+        """
+        pass
 
 
-class VocabularyCustomizer:
-    def __init__(self, nlp):
-        self.nlp = nlp
-
-    def add_word(self, word):
-        self.nlp.vocab.strings.add(word)
-
-    def add_special_case(self, word, case):
-        self.nlp.tokenizer.add_special_case(word, case)
-
-
-class EntityRulerSetup:
-    def __init__(self, nlp):
-        config = {
-            "overwrite_ents": False,
-        }
-        self.ruler = nlp.add_pipe("entity_ruler", config=config, first=True)
-
-    def add_patterns(self, patterns):
-        self.ruler.add_patterns(patterns)
-
-
-class DependencyMatcherSetup:
-    def __init__(self, nlp):
-        self.matcher = DependencyMatcher(nlp.vocab)
-
-    def add_patterns(self, pattern_name, patterns):
-        self.matcher.add(pattern_name, patterns)
+class NERModel(Model):
+    def predict(self) -> List[str]:
+        """
+        Override of the abstract predict method.
+        Specifically returns a list of strings as predictions.
+        """
+        # Placeholder implementation
+        pass
 
 
 # Define a NamedTuple for match details
@@ -91,6 +66,9 @@ class MatchGroup:
             for i in range(start, end):
                 tuples[i] = (match.annotated_text, match.label)
         return tuples
+
+    def __str__(self):
+        return "\n".join([str(x) for x in self.matches])
 
     def __iter__(self):
         return self.matches.__iter__()
@@ -136,6 +114,16 @@ class MatchGroupList(list):
         print("Token labels from matches:")
         [
             (
+                [print("\t" + str(match)) for match in match_group.to()],
+                print("\n"),
+            )
+            for match_group in self
+        ]
+
+    def print_vertical_groups(self):
+        print("Token labels from matches:")
+        [
+            (
                 [
                     print("\t" + str(match))
                     for match in match_group.to_token_text_label_tuples()
@@ -145,25 +133,55 @@ class MatchGroupList(list):
             for match_group in self
         ]
 
+    def __str__(self):
+        return "\n\n".join([str(x) for x in self])
 
-class NLPProcessor:
-    def __init__(self, model="en_core_sci_lg"):
-        self.nlp = spacy.load(model)
-        self.tokenizer_customizer = TokenizerCustomizer(self.nlp)
-        self.vocab_customizer = VocabularyCustomizer(self.nlp)
-        self.entity_ruler_setup = EntityRulerSetup(self.nlp)
-        self.dependency_matcher_setup = DependencyMatcherSetup(self.nlp)
-        self.matcher = self.dependency_matcher_setup.matcher
+
+class RuleBasedTokenClassifier(NERModel):
+    def __init__(self, model="en_core_sci_lg", disable: List = ["ner"]):
+        self.nlp = spacy.load(model, disable=disable)
+        config = {
+            "overwrite_ents": False,
+        }
+        self.ruler = self.nlp.add_pipe("entity_ruler", config=config)
+        self.matcher = DependencyMatcher(self.nlp.vocab)
+        self.span_ruler = self.nlp.add_pipe(
+            "span_ruler",
+            first=True,
+            config={"annotate_ents": True, "overwrite": True},
+        )
+        self.ent_types = set()
+        self.dep_types = set()
+
+    def add_entity_ruler_patterns(self, patterns):
+        self.ruler.add_patterns(patterns)
+        for pattern in patterns:
+            self.ent_types.add(pattern["label"])
+
+    def add_dependency_matcher_patterns(self, pattern_name, patterns):
+        self.matcher.add(pattern_name, patterns)
+        for pattern in patterns:
+            for element in pattern:
+                self.dep_types.add(element["RIGHT_ID"])
+
+    def add_span_patterns(self, patterns):
+        """
+        Adds patterns to the SpanMatcher component.
+        :param patterns: A list of patterns to be added to the SpanMatcher.
+        """
+        self.span_ruler.add_patterns(patterns)
+        for pattern in patterns:
+            self.ent_types.add(pattern["label"])
 
     def process_text(self, text):
         # Process the text with the NLP pipeline
         doc = self.nlp(text)
 
         # Apply the DependencyMatcher
-        matches = self.dependency_matcher_setup.matcher(doc)
+        matches = self.matcher(doc)
 
         # Extract and report entities uniquely
-        lab_entities = {"LAB_RESULT": set(), "LAB_UNIT": set(), "LAB_TYPE": set()}
+        lab_entities = defaultdict(set)
 
         matches = []
         # Matching dependency patterns
@@ -181,13 +199,12 @@ class NLPProcessor:
                     break
             if match:
                 matches.append(match)
-                print(match)
-                print("\n\n")
 
         return lab_entities, doc
 
     def process_text_with_match_groups(self, text):
         doc = self.nlp(text)
+
         reported_tokens = set()
         reported_matches = set()
 
@@ -225,48 +242,48 @@ class NLPProcessor:
 
     def predict(self, text):
         doc = self.nlp(text)
-        reported_tokens = set()
-        reported_matches = set()
 
-        # Modify the logic for extracting and reporting entities
-        lab_entities = {"LAB_RESULT": set(), "LAB_UNIT": set(), "LAB_TYPE": set()}
-        matches = []
-        match_groups = []
+        entity_ruler_matches = []
+        for entity in doc.ents:
+            match_details = Match(
+                label=entity.label_,
+                annotated_text=entity.text,
+                candidate_text=doc.text,
+                start_index=entity.start,
+                end_index=entity.end,
+                match_id=-1,
+            )
+            entity_ruler_matches.append(match_details)
 
-        for match_id, token_ids in self.matcher(doc):
-            matches = []
-            for key in token_ids:
-                entity = doc[key]
-                reported_tokens.add(entity.text)
-                lab_entities[entity.ent_type_].add(entity.text)
-                # Create a MatchDetails instance and add it to matches
-                match_details = Match(
-                    label=entity.ent_type_,
-                    annotated_text=entity.text,
-                    candidate_text=doc.text,
-                    start_index=entity.i,
-                    end_index=entity.i + 1,
-                )
-                if match_details.unique_id in reported_tokens:
-                    matches = []
-                    break
-                reported_tokens.add(match_details.unique_id)
-                matches.append(match_details)
+        match_group_list = MatchGroupList()
+        match_group_list.append(MatchGroup(entity_ruler_matches, doc))
+        return match_group_list.to_combined_label_list()
 
-            if matches:
-                match_group = MatchGroup(matches, doc)
-                if match_group.unique_id not in reported_matches:
-                    reported_matches.add(match_group.unique_id)
-                    match_groups.append(match_group)
-        return match_groups
+    def add_tokenizer_infix_pattern(self, pattern):
+        infixes = pattern
+        infix_regex = compile_infix_regex(infixes)
+        self.nlp.tokenizer.infix_finditer = infix_regex.finditer
+
+    def add_tokenizer_special_case(self, word, case):
+        self.nlp.vocab.strings.add(word)
+        self.nlp.tokenizer.add_special_case(word, case)
+
+    def add_word(self, word):
+        self.nlp.vocab.strings.add(word)
+        self.matcher = DependencyMatcher(self.nlp.vocab)
+
+    def add_special_case(self, word, case):
+        self.nlp.tokenizer.add_special_case(word, case)
+
+    def display_deps(self, text):
+        spacy.displacy.serve(self.nlp(text), style="dep", auto_select_port=True)
 
 
-processor = NLPProcessor()
-processor.vocab_customizer.add_word("mg/dl")
-processor.tokenizer_customizer.add_infix_pattern(r"\.")
-processor.tokenizer_customizer.add_special_case(
-    "mg/dL.", [{ORTH: "mg/dL"}, {ORTH: "."}]
-)
+processor = RuleBasedTokenClassifier()
+processor.add_word("mg/dl")
+processor.add_tokenizer_infix_pattern(r"\.")
+processor.add_tokenizer_special_case("mg/dL.", [{ORTH: "mg/dL"}, {ORTH: "."}])
+processor.add_tokenizer_special_case("mmol/L.", [{ORTH: "mmol/L"}, {ORTH: "."}])
 
 # Define patterns for EntityRuler
 patterns = [
@@ -276,7 +293,7 @@ patterns = [
     {"label": "LAB_TYPE", "pattern": [{"LOWER": {"in": ["cholesterol", "glucose"]}}]},
 ]
 
-processor.entity_ruler_setup.add_patterns(patterns)
+processor.add_entity_ruler_patterns(patterns)
 
 reported_tokens = set()
 # Updated dependency patterns to capture trios, duos, and single entities
@@ -342,34 +359,23 @@ dependency_patterns = [
         }
     ],
     [{"RIGHT_ID": "lab_type", "RIGHT_ATTRS": {"ENT_TYPE": "LAB_TYPE"}}],
+    [{"RIGHT_ID": "lab_result", "RIGHT_ATTRS": {"ENT_TYPE": "diagnosis"}}],
 ]
 
-processor.dependency_matcher_setup.add_patterns("lab_dependency", dependency_patterns)
-
+processor.add_dependency_matcher_patterns("lab_dependency", dependency_patterns)
+# patterns = [
+#     {"label": "diagnosis", "pattern": [{"lower": "malignant"}, {"lower": "melanoma"}]},
+# ]
+# processor.add_span_patterns(patterns)
 
 # Process the text
-text = "The cholesterol level is 200 mg/dL. The glucose result is 120 mmol/L. The test came back positive"
+text = "The cholesterol level is 200 mg/dL. The glucose result is 120 mmol/L. The test came back positive. The patient has malignant melanoma."
 
+print("Match Groups")
+print(processor.process_text_with_match_groups(text))
+print("\n\n")
+prediction = processor.predict(text)
+print("Single list of entity types")
+print(prediction)
 
-print(processor.process_text(text))
-
-
-# # Define a class for match handling
-# class Match:
-#     def __init__(self, match_details: MatchDetails):
-#         self.details = match_details
-#         self.fields = ['label', 'annotated_text', 'candidate_text', 'start_index', 'end_index']
-
-#     def __getitem__(self, item):
-#         return getattr(self.details, item, getattr(self, item))
-
-
-# Define a class for grouping matches
-
-match_group_list = processor.process_text_with_match_groups(text)
-
-# Example usage of MatchGroup
-match_group_list.print_vertical_groups()
-
-print(match_group_list.to_combined_label_list())
-print(match_group_list.to_combined_token_label_tuple_list())
+processor.display_deps(text)
